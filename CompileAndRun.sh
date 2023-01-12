1afa14467bc81b1a3a8233f50a41619da09bef20
mohlondon <baba066h.s@gmail.com>
2023-01-12 13:29:25 +0100

    T

--- CompileAndRun.sh
+++ CompileAndRun.sh
@@ -3,9 +3,9 @@ mkdir .data
 mkdir Logs
 sudo chmod -R a+rwx . # this to edite the permission of this folder and subfolders
 echo "Compiling the Process A ...";
-g++ src/processA.cpp ./src/Myutilities.cpp -lncurses -lrt -lbmp -lm -o bin/ProcessA
+g++ src/processA.cpp ./src/Myutilities.cpp -lrt -lncurses -lbmp -lm -o bin/ProcessA
 echo "Compiling the Process B ...";
-g++ src/processB.cpp ./src/Myutilities.cpp -lncurses -lrt -lbmp -lm -o bin/ProcessB
+g++ src/processB.cpp ./src/Myutilities.cpp -lrt -lncurses -lbmp -lm -o bin/ProcessB
 echo "Compiling the Master Process ...";
 g++ src/master.cpp ./src/Myutilities.cpp  -o bin/master
 echo "Start Runing ...";
